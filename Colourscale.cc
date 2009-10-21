075b2ed6e5a97cac4882671cb167e951b5c81faf
Robert Blake <rob.c.blake.3@gmail.com>
2009-10-21 19:29:49 +0000

    Adding a new color scale to match the default matlab color scale.

--- Colourscale.cc
+++ Colourscale.cc
@@ -120,6 +120,34 @@ void Colourscale :: scale( CScale_t cs )
         cmap[i][2] = 0;
       }
       break;
+    case CS_MATLAB:
+      intrvl = ispan/8.;
+      for ( ; i<intrvl; i++ ) {
+        cmap[i][0] = 0;
+        cmap[i][1] = 0;
+        cmap[i][2] = 0.5 + (float)(i)/(2*intrvl);
+      }
+      for ( ; i<3*intrvl; i++ ) {
+        cmap[i][0] = 0;
+        cmap[i][1] = (float)(i-intrvl)/(2*intrvl);
+        cmap[i][2] = 1;
+      }
+      for ( ; i<5*intrvl; i++ ) {
+        cmap[i][0] = (float)(i-3*intrvl)/(2*intrvl);
+        cmap[i][1] = 1;
+        cmap[i][2] = (float)(5*intrvl-i)/(2*intrvl);
+      }
+      for ( ; i<7*intrvl; i++ ) {
+        cmap[i][0] = 1;
+        cmap[i][1] = (float)(7*intrvl-i)/(2*intrvl);
+        cmap[i][2] = 0;
+      }
+      for ( ; i<n; i++ ) {
+        cmap[i][0] = 0.5 + (float)(8*intrvl-i)/(2*intrvl);
+        cmap[i][1] = 0;
+        cmap[i][2] = 0;
+      }
+      break;
     case CS_BL_RAINBOW:
     default:
       intrvl = ispan/6.;
