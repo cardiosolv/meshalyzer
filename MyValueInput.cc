ad182f2142c73b9ea3d685d694c09f4b4cedcc84
Edward Vigmond <edward.vigmond@u-bordeaux.fr>
2014-03-27 12:12:50 +0000

    Input widgets that control the object to highlight now also respond to the mouse wheel. use SHIFT and CTRL to increase the increment.

--- MyValueInput.cc
+++ MyValueInput.cc
@@ -7,19 +7,41 @@ MyValueInput :: MyValueInput( int X, int Y, int W, int H, const char *L) :
   value();
 }
 
+
+/** A value input widget that also responds to mouse wheel events.
+ *  Holding CTRL moves by 10, SHIFT by 100, and both by 1000
+ *
+ * \param event event type
+ *
+ */
 int MyValueInput :: handle( int event )
 {
   int jump=1;
 
-  if ( event == FL_KEYBOARD ) {
+  if (event == FL_MOUSEWHEEL) {
+
+    if ( Fl::event_state(FL_CTRL) )
+      jump *= 10;
+    if ( Fl::event_state(FL_SHIFT) )
+      jump *= 100;
+
+    int dy = Fl::event_dy();
+    double v1 = value();
+    v1 = increment(v1, dy*jump); 
+    v1 = clamp(v1); 
+    value(v1); 
+    return 1; 
+
+  }  else if ( event == FL_KEYBOARD ) {
+
     if ( Fl::event_state(FL_CTRL) )
       jump = 10;
     if ( Fl::event_state(FL_SHIFT) )
       jump = 100;
 
     switch (Fl::event_key()) {
-      case FL_Up:
-        if ( value()+jump*step() <= maximum() ) {
+        case FL_Up:
+            if ( value()+jump*step() <= maximum() ) {
           value(increment(value(), jump));
           do_callback();
           return 1;
