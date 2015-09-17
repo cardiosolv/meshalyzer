612f054e53ce3f02787713d098a2ca2f059db367
Edward Vigmond <edward.vigmond@u-bordeaux.fr>
2015-09-17 20:02:45 +0000

    More colour bars graciously added by Dr. Boyle

--- Colourscale.h
+++ Colourscale.h
@@ -10,23 +10,23 @@
 #include "PNGwrite.h"
 
 typedef enum {
-  CS_HOT, CS_GREY, CS_RAINBOW, CS_BL_RAINBOW, CS_COLD_HOT, CS_CG, CS_MATLAB_REV,
-  CS_MATLAB
+  CS_HOT, CS_GREY, CS_RGREY, CS_GGREY, CS_BGREY, CS_IGREY, CS_RAINBOW, CS_BL_RAINBOW, 
+  CS_COLD_HOT, CS_CG, CS_MATLAB_REV, CS_MATLAB, CS_ACID, CS_P2G, CS_VIRIDIS
 } CScale_t;
 
 class Colourscale
 {
   public:
     Colourscale(int ts=64, CScale_t tcs=CS_BL_RAINBOW );
-    void     scale( CScale_t );				// set scale
+    void   scale( CScale_t );				// set scale
     inline CScale_t scale(void){ return scaletype; }
-    void     calibrate( double, double ); 	// set range
+    void   calibrate( double, double ); 	// set range
     inline float min(){return mindat;}		// return minimum data value
     inline float max(){return maxdat;}		// return maximum data value
     void colourize( float );				// colour vector for data value
     void colourize( float, float );			// same as above, specify alpha
     inline int size(){ return n; }			// get the size
-    void     size( int );					// set the size
+    void   size( int );	    				// set the size
     inline GLfloat* entry(int a){return cmap[a];}	// return an entry
     GLfloat *colorvec( double );
     void   output_png( const char * );            // output the map
